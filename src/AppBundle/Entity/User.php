df7de8bd40e171d02aa7cabb21192d1cb872ed88
MaryemBenKbayer <mariem.benkbaier@esprit.tn>
2020-02-26 04:35:06 +0100

    Maryem commit

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use http\Client\Curl\User;
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
